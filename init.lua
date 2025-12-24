326e2d117f4751f9af77a65f513db35c2f147836
ludlgr <ludlgr13@gmail.com>
2025-12-24 21:13:46 +0100

    Disable 's' key binding for an easier use of mini.surround

--- init.lua
+++ init.lua
@@ -205,6 +205,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Disabling native 's' key binding for an easier use of mini.surround
+vim.keymap.set({ 'n', 'x' }, 's', '<Nop>')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
